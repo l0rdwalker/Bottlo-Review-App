09686fb40244d49833b1239a5af1d134854ec903
William <l0rdwalker2oo1@gmail.com>
2021-09-24 13:56:47 +1000

    Altered to have comments included.

--- app/src/main/java/com/example/liquerreviewattempttwo/MainActivity.kt
+++ app/src/main/java/com/example/liquerreviewattempttwo/MainActivity.kt
@@ -34,26 +34,60 @@ class MainActivity : AppCompatActivity() {
             "25/9/2020","Very inconvenient to get to. But they seem to be the only place that sells my favorite flavour of a specific drink for very cheap. Worth it.",
             1, "mentoneliqerclearence")
 
+        /*
+            The above code is responsible for creating the objects which will eventually be passed into view fragments.
+         */
+
         val list = listOf<shopFronts>(BottleMart, Duncans, IGAbentleigh,BoozeMate)
 
-        list.forEachIndexed{index, element ->
-            supportFragmentManager.beginTransaction().apply {
-                var bundle = Bundle()
-                bundle.putParcelable("params", element)
-                var myObj = location_frag()
-                myObj.setArguments(bundle)
+            /*
+            I've put these location data classes into a non-mutable list of objects. Becasue the application doesn't support
+            adding new locations into the system I thought that useing a mutable list was uncessary. Therefore, I didn't. True story.
+            */
 
-                var resourceId = resources.getIdentifier("fragmentContainerView$index", "id", getBaseContext()
-                    ?.getPackageName()
-                )
+        if (savedInstanceState == null) {
 
-                replace(resourceId, myObj)
-                commit()
+            /*
+            I encounted an issue that everytime the application cylced through its various states it would factory reset all fragment objects and their data.
+            So on a screens rotation the star rating bar would reset back to its default state. This is becasue everything the main activity re-ran its
+            oncreate function it would reinitilize all the fragment data. This was annoying, as I had designed fragments to savce their instancechange data and
+            reinitalize irrespective of the main activity. To fix this, I made it so the fragment manager would only create new fragment objects when
+            a savedinstance state is not detected. This allowed fragment data to retain themselves through orientation changes.
+             */
+
+
+            list.forEachIndexed { index, element ->
+                supportFragmentManager.beginTransaction().apply {
+
+                    var bundle = Bundle()
+                    bundle.putParcelable("params", element)
+                    var myObj = location_frag()
+                    myObj.setArguments(bundle)
+
+                    var resourceId = resources.getIdentifier(
+                        "fragmentContainerView$index", "id", getBaseContext()
+                            ?.getPackageName()
+                    )
+
+                    replace(resourceId, myObj)
+                    commit()
+
+                    /*
+                    While the use of fragments was not outlined within the project specifiycation document, I though their implermentation made sense for this application.
+                    Online, one of the usecases of fragments are they they allow you to create reuseable UI elements. As this project has four structually identiical
+                    onscreen elements to display location data I thought their implermentation made sense. However, this ended up causeing alot of headaces. So,
+                    neadless to say I have regrets. More information reguarding this can be foud in my GAP report.
+                 */
+
+                    /*
+                    Location information (stored as objects) are saved as a bundle which is then passed into the myObj fragment object. This allows each fragment
+                    to have access to the data is needs to display the relevent onscreen information.
+                    */
+                }
             }
         }
 
-
-
-
     }
+
+
 }
--- app/src/main/java/com/example/liquerreviewattempttwo/location_frag.kt
+++ app/src/main/java/com/example/liquerreviewattempttwo/location_frag.kt
@@ -16,12 +16,25 @@ import androidx.fragment.app.Fragment
 
 class location_frag() : Fragment() {
     var StoreInformation: shopFronts? = null
+    var ViewSave: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val args = arguments
-        StoreInformation = args?.getParcelable<shopFronts>("params")
+
+        if (savedInstanceState != null) {
+            StoreInformation = savedInstanceState.getParcelable("Storeinfo")
+            StoreInformation?.let { ViewSave?.let { it1 -> StarSelect(it.Stars, it1) } }
+        } else {
+            val args = arguments
+            StoreInformation = args?.getParcelable<shopFronts>("params")
+        }
+        /*
+        For the mostpart, the oncreate method here checks to see if there is a savedinstance and loads in the saved parcelable object it inilizes it.
+        If there isn't a savedinstance it will load the object passed through by the main activity at the fragments initial creation.
+        */
+
     }
+
 
     val startForResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -31,12 +44,20 @@ class location_frag() : Fragment() {
                     val TempObject = data?.getParcelableExtra<shopFronts>("ReturnedObject")
                     StoreInformation = TempObject
                     Log.i(StoreInformation?.Stars.toString(),StoreInformation?.Stars.toString())
-
+                    StoreInformation?.let { ViewSave?.let { it1 -> StarSelect(it.Stars, it1) } }
                 }
             }
         }
+    /*
+The start for result loads in the data bassed back from the detailed activity, It'll then load the changed data (specifically the star rating provided by the user)
+*/
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        /*
+The onCreateView loads in the provided view information. It is also here where the majority of the onscreen information relevent to any store location is
+substituted into the placeholder widgets. E.g. the store name, address, default star rating ect.
+*/
+
         val view: View = inflater!!.inflate(R.layout.fragment_location_frag, container, false)
         val name = view.findViewById<TextView>(R.id.Title)
         name.setText(StoreInformation?.Name)
@@ -45,9 +66,7 @@ class location_frag() : Fragment() {
         Address.setText(StoreInformation?.Address)
 
         val Image = view.findViewById<ImageView>(R.id.PreviewImage)
-        val resourceId = this.resources.getIdentifier(StoreInformation?.Picture.toString(), "drawable", activity?.getBaseContext()
-            ?.getPackageName()
-        )
+        val resourceId = this.resources.getIdentifier(StoreInformation?.Picture.toString(), "drawable", activity?.getBaseContext()?.getPackageName())
         Image.setImageResource(resourceId)
 
         val Button = view?.findViewById<Button>(R.id.MoreInfoBTN)
@@ -56,10 +75,14 @@ class location_frag() : Fragment() {
                 putExtra("StoreInfo", StoreInformation)
             }
             startForResult.launch((intent))
+
+            /*
+When the more infromation button is clicked, a onclick listener saves the store object information into an intent and passes it through to the detailed activity.
+*/
         }
 
         StoreInformation?.let { StarSelect(it.Stars,view) }
-        Log.i("onCreateView","onCreateView")
+        ViewSave = view
         return view
     }
 
@@ -75,6 +98,19 @@ class location_frag() : Fragment() {
                 SetStar.setImageResource(R.drawable.nostar)
             }
         }
-        StoreInformation?.Stars = StarNum
+
+        /*
+This method takes the store object value that relates to the star rating, and ajusts the onscreen display to reflext it. It checks each star and sets any
+star with a value less or equal to the set value as being the filled in star, any star with a value greater then the set value is set to display the no star
+drawable.
+*/
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable("Storeinfo",StoreInformation)
+        /*
+Saves the current object which contains store information for when the activity lifecyle loops through.
+*/
     }
 }
